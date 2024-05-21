ca9f14745f8df5b29c48098325c8b13caadc392e
Nahom20 <nahommule1738@gmail.com>
2024-05-21 17:46:03 -0500

    THe Withdrawl and transfer

--- project/banking/domain/BankAccount.java
+++ project/banking/domain/BankAccount.java
@@ -1,19 +1,17 @@
 package banking.domain;
 
+import banking.data.BankingAccountDAO;
 import framework.domain.Account;
 import framework.domain.Customer;
 
-public class BankAccount extends Account {
+public abstract class BankAccount extends Account {
 
 
 	public BankAccount(String accountNumber, Customer customer) {
         super(accountNumber, customer);
 	}
+	private BankingAccountDAO bankingAccountDAO;
 
-	@Override
-	public void withdraw(double amount) {
-
-	}
 
 	@Override
 	public void transferFunds(Account toAccount, double amount, String description) {
