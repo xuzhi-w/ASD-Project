3c1757a2b8fdc50a6183ba8ad342702b0c3ab986
mkoner <amzid@hotmail.fr>
2024-05-21 16:25:38 -0500

    Implemented the DAO classes in banking and creditcard

--- project/framework/banking/data/BankingAccountDAO.java
+++ project/framework/banking/data/BankingAccountDAO.java
@@ -3,27 +3,38 @@ package framework.banking.data;
 import framework.data.AccountDAO;
 import framework.domain.Account;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class BankingAccountDAO implements AccountDAO {
+    Collection<Account> accountlist = new ArrayList<>();
     @Override
     public void saveAccount(Account account) {
-
+        accountlist.add(account);
     }
 
     @Override
     public void updateAccount(Account account) {
-
+        Account accountexist = loadAccount(account.getAccountNumber());
+        if (accountexist != null) {
+            accountlist.remove(accountexist); // remove the old
+            accountlist.add(account); // add the new
+        }
     }
 
     @Override
     public Account loadAccount(String accountnumber) {
+        for (Account account : accountlist) {
+            if (account.getAccountNumber().equals(accountnumber)) {
+                return account;
+            }
+        }
         return null;
     }
 
     @Override
     public Collection<Account> getAccounts() {
-        return List.of();
+        return accountlist;
     }
 }
